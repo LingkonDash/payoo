063474d218665fbba6ea7661136d3786138c6a0f
LingkonDash <cocrupon@gamil.com>
2026-02-19 21:50:19 +0600

    Added Nav bar

--- scripts/login.js
+++ scripts/login.js
@@ -4,11 +4,15 @@ btn.addEventListener('click', () => {
   const number = getEelemValueByinput('mobile-num');
   const pin = getEelemValueByinput('login-pin');
   
+  if(isNaN(number)) {
+    alert('Please input Numbers only'); 
+    return;
+  }
   if (number.length !== 11) {
     alert('Number must contain 11 digits')
     return;
   }
   if (pin.length !== 4) { alert('Please insert 4 digit pin'); return}
-  if (pin !== '1234') { alert(`Wrong Pin! Right pin is 1234 🙂`); return }
+  if (pin !== '1234') { alert('Wrong Pin! Right pin is 1234 🙂'); return }
   window.location.assign('home.html');
 })
